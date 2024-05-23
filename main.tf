db42890903db345126550e68a4bdf6f957f0a206
realexcel2021 <realexcel2020@gmail.com>
2024-05-23 22:00:09 +0100

    update

--- main.tf
+++ main.tf
@@ -17,5 +17,5 @@ locals {
   domain_name = "devopslord.com" # trimsuffix(data.aws_route53_zone.this.name, ".")
   subdomain   = "ha-serverless"
   region1 = "us-east-1"
-  region2 = "us-weat-1"
+  region2 = "us-west-1"
 }
